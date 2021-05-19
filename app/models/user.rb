0d2566d2d7a647b0c8773464be85b8aad6db9d20
shota-hibino <yamoushou@gmail.com>
2021-05-19 13:05:05 +0000

    [Update]function comment add

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # アソシエート設定
   has_many :books, dependent: :destroy
   attachment :profile_image
 
